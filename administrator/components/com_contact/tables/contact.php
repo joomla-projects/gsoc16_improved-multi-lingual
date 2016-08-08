a1b424b563906f4e45120f67a1e3834fc09ca078
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-08-08 11:18:19 +0100

    add override to field mapping in com_contact Contact item JTable

--- administrator/components/com_contact/tables/contact.php
+++ administrator/components/com_contact/tables/contact.php
@@ -19,6 +19,28 @@ use Joomla\Registry\Registry;
 class ContactTableContact extends JTable
 {
 	/**
+	 * Mapping of most used fields in the table.
+	 *
+	 * @var    array
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $fieldsMapping = array(
+		'id'               => 'id',
+		'title'            => 'name',
+		'alias'            => 'alias',
+		'published'        => 'state',
+		'access'           => 'access',
+		'language'         => 'language',
+		'catid'            => 'catid',
+		'menutype'         => null,
+		'ordering'         => 'ordering',
+		'level'            => null,
+		'created_by'       => 'created_by',
+		'checked_out'      => 'checked_out',
+		'checked_out_time' => 'checked_out_time',
+	);
+
+	/**
 	 * Ensure the params and metadata in json encoded in the bind method
 	 *
 	 * @var    array
